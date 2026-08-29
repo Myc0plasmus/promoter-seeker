3ee72a33a9dd2484bfebeb39455c2344682ec12a
Myc0plasmus <jablonski.natan@gmail.com>
2026-08-29 08:54:38 +0000

    in progress

--- src/promoter_seeker/main.py
+++ src/promoter_seeker/main.py
@@ -1,5 +1,12 @@
+import pandas as pd
+from pathlib import Path
+
+
 def main():
-    print("Hello world!")
+    path = Path.cwd() / "promotory.csv"
+    df = pd.read_csv(path,sep=";")
+    print(df)
+
 
 if __name__ == "__main__":
     main()
